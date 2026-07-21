eeb0e42dc1ecdb2893bb71b0fd8230ad5b78bcde
sanjay mani <sanjaymani9443@gmail.com>
2026-07-22 00:01:57 +0530

    table_first

---
+++ table_first_sql.sql
@@ -0,0 +1,14 @@
+create database samplefirstdata;
+
+use samplefirstdata;
+create table person (
+personID int ,
+Lastname varchar(255) not null,
+Firstname varchar(255),
+Address varchar(255),
+city varchar(255)
+);
+
+select*from person;
+
+show databases;
