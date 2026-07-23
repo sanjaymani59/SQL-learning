1edd81ad69648afda7afc9dd8c6e26803a9e7756
sanjay mani <sanjaymani9443@gmail.com>
2026-07-24 02:20:03 +0530

    updated sql

--- table_first_sql.sql
+++ table_first_sql.sql
@@ -18,8 +18,43 @@ insert into person(personID,Lastname,city)
 values(104,"danc","pollachi");
 
 select* from person where personid !=101;
+set sql_safe_updates=0;
+update person
+set Lastname="samm"
+where personID="101";
 
+delete from person
+where personID="104";
+
+set autocommit=0;
+
+rollback;
+
+commit;
 
 select * from person;
 
+
+
 show databases;
+
+create table hello(
+		 dated date,
+         tad  datetime,
+         tim time
+         );
+         
+insert into hello
+values(current_date(),now(),current_time());
+
+commit;
+rollback;
+         
+         
+         
+select*from hello
+         
+
+
+
+
