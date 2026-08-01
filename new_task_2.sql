7432fe1efb88e2a73afd0f7b3311bff715c31a8b
sanjay mani <sanjaymani9443@gmail.com>
2026-08-02 02:29:58 +0530

    task 02

---
+++ new_task_2.sql
@@ -0,0 +1,14 @@
+create database garlic;
+
+use garlic;
+create table workers(
+	Name_ varchar(40),
+    place_ varchar(40),
+    dailay_wages int);
+    
+    
+insert into workers values ('ram','bokka',5999);
+    
+select*from workers;
+
+
