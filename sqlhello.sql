917295cb429f2ac34569f9732c45ef38a5d54dc4
sanjay mani <sanjaymani9443@gmail.com>
2026-08-08 03:03:29 +0530

    hello sql

---
+++ sqlhello.sql
@@ -0,0 +1,12 @@
+create database hello;
+
+create table timess(
+   Da_te date,
+   dAt datetime,
+   neram time);
+
+use timmess;
+
+insert into timess values(current_date(),now(), current_time());
+
+select*from timess;
