477287cb76f5d84440ab0f801fb6ea3e4566d3c0
sanjay mani <sanjaymani9443@gmail.com>
2026-08-08 22:03:39 +0530

    tranction

---
+++ tranction.sql
@@ -0,0 +1,14 @@
+create table tranction(
+	t_id int primary key auto_increment,
+	salary decimal(6,4));
+
+select*from tranction;
+drop table tranction;
+
+insert into tranction(salary)values(79.4);
+insert into tranction(salary)values(69.4);
+insert into tranction(salary)values(59.4);
+insert into tranction(salary)values(89.4);
+insert into tranction(salary)values(99.4);
+
+
