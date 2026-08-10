82ed9f2460a7140feedd13a554b486bf803ce8a1
sanjay mani <sanjaymani9443@gmail.com>
2026-08-10 23:06:50 +0530

    simple table

--- sqlhello.sql
+++ sqlhello.sql
@@ -10,3 +10,18 @@ use timmess;
 insert into timess values(current_date(),now(), current_time());
 
 select*from timess;
+
+
+create table helloo(
+name_ varchar(40),
+ag_e int(100),
+passion varchar(40),
+hobbi varchar(30));
+
+insert into helloo values
+('hari',45,'simply setting','no hobbi');
+
+select*from helloo;
+
+
+
