6f6f0bb1d3305aa8cb00ab869ef98606f02cc060
sanjay mani <sanjaymani9443@gmail.com>
2026-08-15 22:54:48 +0530

    sql database

--- imported_database.sql
+++ imported_database.sql
@@ -2,5 +2,27 @@ use employee;
 
 select*from for_database;
 drop table employee;
+select*from 
+for_database where payment_method like '%c'; 
+select*from 
+for_database where payment_method like 'c%'; 
+
+select*from 
+for_database where payment_method regexp 'COD'; 
+select*from 
+for_database where payment_method regexp '[CO]D'; 
+select*from 
+for_database where payment_method regexp 'C[OD]'; 
+
+select*from 
+for_database where payment_method regexp '^COD$|mac|hello'; 
 
 select product_category from for_database;
+
+select*from for_database where product_category 
+regexp 'beauty';
+select*from for_database where product_category 
+like '%e';
+
+
+
