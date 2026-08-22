26877c82b05bc448863e6525db15746849133b45
sanjay mani <sanjaymani9443@gmail.com>
2026-08-22 22:04:26 +0530

    primary_key new

---
+++ primary_key.sql
@@ -0,0 +1,27 @@
+create database primary_key;
+
+use primary_key;
+create table drivers(
+driver_id int primary key,
+driver_name varchar(100),
+license_number varchar(50) unique );
+insert into drivers(driver_id,driver_name,license_number) 
+values(1,101,'sam');
+
+
+create table riders(
+rider_id int primary key,
+driver_id int,
+pickup_location varchar(50),
+dropoff_location varchar(50),
+ride_date datetime,
+fare decimal(10,3),
+foreign key (driver_id) references deivers(driver_id));
+
+insert into riders(rider_id,driver_id,pickup_location,
+dropoff_location,ride_date,fare) values(1,101,'chennai',
+'coimbator','2024-12-29 08:00:00',500.00);
+ 
+drop table riders;
+drop table drivers;
+select*from riders;
