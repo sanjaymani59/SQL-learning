af1318b29a78891771c2d78db2b8d9634b1354f8
sanjay mani <sanjaymani9443@gmail.com>
2026-08-23 22:48:29 +0530

    sql

--- new_task_2.sql
+++ new_task_2.sql
@@ -2,15 +2,26 @@ create database garlic;
 
 use garlic;
 create table workers(
-	Name_ varchar(40),
-    place_ varchar(40),
-    dailay_wages int);
+	Name_ varchar(40) unique,
+    place_ varchar(40) unique,
+    dailay_wages int,
+    emp_age int primary key
+    );
     
     
-insert into workers values ('ram','bokka',5999,05-07-2026);
-alter table workers add Date_of_work date;
+insert into workers values
+('sam','bokk','2',101);
+    
+    
+    
+    
+    
+select*from workers;
 
-alter table workers add month_of_work varchar(9);
+drop table workers;
+
+
+
 
 
 
