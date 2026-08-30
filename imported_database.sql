610088c9a141d19969bbc7ebf002020a3b763686
sanjay mani <sanjaymani9443@gmail.com>
2026-08-30 21:08:24 +0530

    lmodified data

--- imported_database.sql
+++ imported_database.sql
@@ -8,40 +8,42 @@ select*from
 for_database where payment_method like 'c%'; 
 
 select*from 
-for_database where payment_method regexp 'COD'; 
+imported_data where payment_method regexp 'COD'; 
 select*from 
-for_database where payment_method regexp '[CO]D'; 
+imported_data where payment_method regexp '[CO]D'; 
 select*from 
 for_database where payment_method regexp 'C[OD]'; 
 
 select*from 
-for_database where payment_method regexp '^COD$|mac|hello'; 
+imported_data where payment_method regexp '^COD$|mac|hello'; 
 
 select product_category from for_database;
 
-select*from for_database where product_category 
+select*from imported_data where product_category 
 regexp 'beauty';
 select*from for_database where product_category 
 like '%e';
-select*from for_database order by unit_price asc;
+select*from imported_data order by unit_price asc;
 select*from for_database order by delivery_days asc;
 
-select*from for_database where product_category ='home' or product_category ='beauty';
+select*from imported_data where product_category ='home' or product_category ='beauty';
+
+select*from imported_data where product_category ='home' and product_category ='beauty';
 
 select*from for_database where product_category regexp '%s';
 
 
 
-select*from for_database where product_category in ('home','beauty','clothing');
+select*from imported_data where product_category in ('home','beauty','clothing');
 
 
 select customer_id ,sum(revenue) 
 as total_spend, dense_rank() over (order by sum(revenue)  desc) 
-as spend_rank from for_database group by customer_id;
+as spend_rank from imported_data group by customer_id;
 
 alter table for_database drop  summa;
 
-desc for_database;
+desc imported_data;
 
 select *from for_database where payment_method in ('card','wallet');
 alter table for_database rename to imported_data;
@@ -58,7 +60,7 @@ select*from imported_data where unit_price>30  and  region ='east' and product_c
 
 select count(*) from imported_data where product_category ='Clothing' ;
 select count(*) from imported_data where product_category ='Clothing'and payment_method= 'card';
-select count(*) from imported_data where product_category in('Clothing','home');
+select count(*) from imported_data where product_category in ('Clothing','home');
 select count(*) from imported_data where product_category not in('Clothing','home');
 
 select sum(unit_price) from imported_data where  product_category ='Clothing';
