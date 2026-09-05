fa78bcf9cff78defa121ae0d2801f8271367c28b
sanjay mani <sanjaymani9443@gmail.com>
2026-09-05 22:21:44 +0530

    simple upper and lower

--- student_database.sql
+++ student_database.sql
@@ -6,16 +6,19 @@ student_id int primary key,
 student_name varchar(50),
 student_department varchar(30),
 student_year int,
-student_mark int );
+student_mark int,
+Da_te date,
+dAt datetime,
+neram time);
 
 insert into students values
-(1,'ragul','cse',3,78),
-(2,'naveen','ece',3,89),
-(3,'kavi','cse',3,80),
-(4,'raja','tt',3,90),
-(5,'deva','tt',3,70),
-(6,'logesh','tt',3,70),
-(7,'monish','cse',3,85);
+(1,'ragul','cse',3,78,current_date(),now(),current_date()),
+(2,'naveen','ece',3,89,current_date(),now(),current_date()),
+(3,'kavi','cse',3,80,current_date(),now(),current_date()),
+(4,'raja','tt',3,90,current_date(),now(),current_date()),
+(5,'deva','tt',3,70,current_date(),now(),current_date()),
+(6,'logesh','tt',3,70,current_date(),now(),current_date()),
+(7,'monish','cse',3,85,current_date(),now(),current_date());
 
 update students set student_name ='ramana' where student_id=4;
 
@@ -47,6 +50,9 @@ select*from students where student_year=3;
 
 select*from students where student_email='logesh@gmail.com';
 
+
+select length(student_name) as lengthh  ,upper(student_name) from students;
+select length(student_name) as lengthh  ,lower(student_name) from students;
 
 
 
