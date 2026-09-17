28b71ce8bd91c2ec7daaa136bd0cc59e3a6a839d
sanjay mani <sanjaymani9443@gmail.com>
2026-09-17 23:36:57 +0530

    new to sql

--- task_02.sql
+++ task_02.sql
@@ -11,3 +11,32 @@ student left join  stuffs on student.student_id =stuffs.stuffs_id;
 
 select name, student.departament, marks  from 
 student right join  stuffs on student.student_id =stuffs.stuffs_id;
+
+drop table users;
+create table users(
+	ID int not null auto_increment,
+    NAME varchar(50) not null ,
+    age int not   null,
+    primary key(ID)
+);
+alter table users add Gender varchar(59) not null after age;
+alter table users add city varchar(34) not null, add contant varchar(50);
+alter table users modify city varchar(50) not null ;
+alter table users rename to sam ;
+insert into  sam values(null,'am',34,'male','chennai','4567');
+
+insert into sam  ( name,age,gender,city,contant) values ('jayam',45,'female','ooty','4567');
+
+select*from sam;
+describe users;
+show tables;
+
+
+https://youtu.be/cpbd7CLAqtw?si=yWZhdwfrFdUMW0O_
+21;49
+
+
+
+
+
+
