cd1ce7e8a46548d658454f1b6bb599cb2e5caeed
sanjay mani <sanjaymani9443@gmail.com>
2026-09-20 00:08:58 +0530

    modified

--- task_02.sql
+++ task_02.sql
@@ -26,13 +26,25 @@ alter table users rename to sam ;
 insert into  sam values(null,'am',34,'male','chennai','4567');
 
 insert into sam  ( name,age,gender,city,contant) values ('jayam',45,'female','ooty','4567');
+insert into sam  ( name,age,gender,city,contant) values ('kavi',34,'male','kulla','9845498');
+insert into sam  ( name,age,gender,city,contant) values ('raj',99,'male','krish','4546952');
+insert into sam  ( name,age,gender,city,contant) values ('samji',39,'female','sivi','123849');
+insert into sam  ( name,age,gender,city,contant) values ('dd',98,'female','erode','456982');
+insert into sam  ( name,age,gender,city,contant) values ('sd',32,'female','salem','55662222225');
+delete from sam where id =4;
 
 select*from sam;
 describe users;
 show tables;
 delete from sam where id= 1;
 
-update sam set 
+update sam set city='houser', contant='89747859' where id= 5;
+
+truncate table sam ;
+select name,age,city from sam where city ='ooty';
+select name,age,city from sam where city ='kulla' and  age>=34 ;
+select name,age,city from sam where city ='kulla' or city='ooty' and age>=25 order by city;
+
 
 
 
